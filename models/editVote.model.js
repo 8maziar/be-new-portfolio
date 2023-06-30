3407d6ac9aad073f95a8b6b41d70449cc0ec903b
8maziar <8maziar@gmail.com>
2023-06-30 11:13:57 +0100

    update qury

--- models/editVote.model.js
+++ models/editVote.model.js
@@ -1,8 +1,15 @@
 const db = require("../db/connection");
 
 exports.editVote = (id, inc_votes) => {
-  return db.query(`SELECT votes FROM comments WHERE comment_id = $1;`, [id]).then(({ rows }) => {
-    const sum = rows[0].votes + inc_votes;
-    return db.query(`UPDATE comments SET votes = $1 WHERE comment_id = $2 RETURNING *;`, [sum, id]).then(({ rows }) => rows[0]);
-  });
+  return db
+    .query(
+      `
+    UPDATE comments
+    SET votes = votes + $1
+    WHERE comment_id = $2
+    RETURNING *;
+  `,
+      [inc_votes, id]
+    )
+    .then(({ rows }) => rows[0]);
 };
